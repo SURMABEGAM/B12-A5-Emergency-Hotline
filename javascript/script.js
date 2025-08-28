f0ed1b82a462e5889d24efd6c0d2f164b210c296
Surma Begam <sbtisha2001@gmail.com>
2025-08-28 19:57:04 +0600

    Implement button functionality for hostory

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,52 @@
 let count = 0;
 let countDisplay = document.getElementById("count");
 
-// get all elements with class "increase"
+// classNAME "increase"
 let increaseButtons = document.getElementsByClassName("increase");
 
-// loop through all 6 elements
 for (let i = 0; i < increaseButtons.length; i++) {
     increaseButtons[i].addEventListener("click", function () {
         count++;
         countDisplay.textContent = count;
     });
 }
+
+let coins =110; 
+const coinDisplay = document.getElementById("coin");
+
+const callButtons = document.querySelectorAll(".decrease");
+const callHistory = document.getElementById("call-history");
+
+callButtons.forEach(button => {
+  button.addEventListener("click", function() {
+    if (coins < 20) {
+      alert("You do not have enough coins.");
+      return;
+    }
+
+    const card = button.closest(".bg-white");
+    const serviceName = card.querySelector("h2").innerText;
+    const serviceNumber = card.querySelector("p.text-xl").innerText;
+
+    // Alert দেখানো
+    alert(`Calling ${serviceName} at ${serviceNumber}`);
+    coins -= 20;
+    coinDisplay.textContent = coins;
+
+    // Call History-name + number + time)
+    const historyItem = document.createElement("div");
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    historyItem.innerHTML = `<strong>${serviceName}</strong> - ${serviceNumber} <span class="time">(${timeString})</span>`;
+    callHistory.appendChild(historyItem);
+  });
+});
+
+// Clear History button
+const clearBtn = document.getElementById("clear-history");
+const callHistorys = document.getElementById("call-history");
+
+clearBtn.addEventListener("click", function() {
+  callHistory.innerHTML = ""; 
+});
+
